f8fdb0229e31b9c1a549d036964c9d0c3188ca03
And21rew <Andrei.Bubnov@urfu.me>
2021-07-07 09:08:41 +0500

    Commit 3 player
    The basis of the playerr is made

---
+++ Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpHeight;
+    public Transform checkGround;
+    bool isGrounded;
+    Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroundCheck();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+
+        if (Input.GetAxis("Horizontal") == 0 && (isGrounded))
+        {
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            Flip();
+            if (isGrounded)
+            {
+                anim.SetInteger("State", 2);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+        if (Input.GetAxis("Horizontal") < 0)
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    void GroundCheck()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(checkGround.position, 0.2f);
+        isGrounded = colliders.Length > 1;
+        if (!isGrounded)
+            anim.SetInteger("State", 3);
+    }
+}
