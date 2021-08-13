e8d7ddce21de5935794f8b354a9c5e375b774f16
And21rew <Andrei.Bubnov@urfu.me>
2021-08-13 12:24:46 +0500

    Commit 22 new lvls

--- Platformer/Assets/Scripts/Button.cs
+++ Platformer/Assets/Scripts/Button.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    public GameObject[] block;
-    public Sprite btnActive;
+    [SerializeField] private GameObject[] block;
+    [SerializeField] private Sprite btnActive;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "MarkBox")
+        if (collision.gameObject.CompareTag("MarkBox"))
         {
             GetComponent<SpriteRenderer>().sprite = btnActive;
             GetComponent<CircleCollider2D>().enabled = false;
             foreach (GameObject obj in block)
-            {
                 Destroy(obj);
-            }
         }
     }
 }
---
+++ Platformer/Assets/Scripts/Trap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField] private GameObject[] ActiveBlocks;
+    [SerializeField] private GameObject[] DestroyBlocks;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            foreach (GameObject obj in ActiveBlocks)
+                obj.SetActive(true);
+
+            foreach (GameObject obj in DestroyBlocks)
+                Destroy(obj);
+        }
+    }
+}
